d428d652d1cb0e5067f3d8fe3ed4fe6abf12ab0c
Severin Buchser <severin.buchser@gmx.ch>
2021-05-26 20:54:59 +0200

    Update dependencies

--- projects/sb-element/schematics/ng-add/dependencies/dependencies.ts
+++ projects/sb-element/schematics/ng-add/dependencies/dependencies.ts
@@ -1,6 +1,6 @@
 import { NodeDependency, NodeDependencyType } from 'schematics-utilities';
 
 export const dependencies: Array<NodeDependency> = [
-  { type: NodeDependencyType.Default, version: '~1.0.2', name: 'sb-element' },
+  { type: NodeDependencyType.Default, version: '~1.0.4', name: 'sb-element' },
   { type: NodeDependencyType.Default, version: '~1.0.0', name: 'sb-theming' }
 ];
