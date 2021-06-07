2ec639b64b6bce2a3881169e9fa16fd210e879e2
Severin Buchser <severin.buchser@gmx.ch>
2021-06-07 05:41:24 +0200

    Add alert service general documentation

--- projects/sb-element/src/lib/services/alert/alert.service.ts
+++ projects/sb-element/src/lib/services/alert/alert.service.ts
@@ -3,6 +3,43 @@ import { Alert } from '../../models/alert/alert';
 import { AlertServiceSubscriber } from '../../models/alert/alert-service-subscriber';
 import { Queue } from '../../models/queue/queue';
 
+/**
+ * Provides a global service to display alerts.
+ *
+ * The alert service can be used by any component which either whishes to show
+ * or to create an alert to be shown.
+ *
+ * If a component wants to show alerts, the
+ * component has to call [subscribe]{@link #subscribe} with the component as the
+ * parameter. The component also needs to implement the
+ * {@link AlertServiceSubscriber} interface so the service can inform the
+ * subscriber about the incoming alerts.
+ *
+ * If a component wants to create an alert, the service provides different
+ * methods which have predefined colors. The different methods take a `message`
+ * which is a `string` and an optional `size` parameter, which is also a
+ * `string`. The methods then passes the alert to the
+ * {@link AlertServiceSubscriber} with a predefinded color. There is also a
+ * method, which lets you customize everything: [alert]{@link #alert}. All these
+ * methods then add the created alert to the [alertQueue]{@link #alertQueue},
+ * and then call [work]{@link #work}. This method times the alerts and then
+ * informs the subscriber when a alert needs to be shown (when an alert is
+ * ready to be shown).
+ *
+ * The lifecycle of an {@link Alert} inside the service:
+ * 1. The {@link Alert} gets created via a create call (like
+ * [alert]{@link #alert}).
+ * 2. The {@link Alert} gets placed into the [alertQueue]{@link #alertQueue}.
+ * 3. The [work]{@link #work} method then removes an {@link Alert} out of the
+ * queue and tells the subscribers to show the removed {@link Alert}. The
+ * {@link Alert} gets collected by the garbage collector after the {@link Alert}
+ * has been shown.
+ * 4. If all subscribers have finished showing the {@link Alert}, the work
+ * method checks if the [alertQueue]{@link #alertQueue} is empty or not. If the
+ * queue is empty, the [work]{@link #work} method terminates. If not, go to step
+ * 3 again.
+ *
+ */
 @Injectable({
   providedIn: 'root'
 })
