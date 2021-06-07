db4bd8e7ea7309d6cdc5020e6eaffd2cf3f0dd1d
Severin Buchser <severin.buchser@gmx.ch>
2021-06-07 04:42:37 +0200

    Document theme service

--- projects/sb-element/src/lib/services/theme/theme.service.ts
+++ projects/sb-element/src/lib/services/theme/theme.service.ts
@@ -1,12 +1,38 @@
 import { Injectable } from '@angular/core';
 
+/**
+  * A service which provides global access to the current theme.
+  *
+  * The service allows a component to get and set the theme. There are two
+  * themes:
+  * - `light`
+  * - `night`
+  *
+  * The default theme is the `night` theme. The theme is stored in form of a
+  * string, which is then included into classes of the different components or
+  * elements.
+  */
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
-
+  /**
+   * The current theme. The default theme is `night`.
+   */
   private _theme: string = 'night';
-  public commit(theme: string) { this._theme = theme }
+
+  /**
+   * Sets the theme to the service.
+   *
+   * @param{string} The theme to commit
+   */
+  public commit(theme: string): void { this._theme = theme }
+
+  /**
+   *  Gets the current theme.
+   *
+   * @returns{string} The current theme
+   */
   public get(): string { return this._theme }
 
 }
