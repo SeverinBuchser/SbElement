c385dd4ccc055893c704b5575a55b39e5c48535a
Severin Buchser <severin.buchser@gmx.ch>
2021-06-08 16:02:42 +0200

    Add Alert interface documentation

--- projects/sb-element/src/lib/models/alert/alert.ts
+++ projects/sb-element/src/lib/models/alert/alert.ts
@@ -1,5 +1,23 @@
+
+/**
+ * This interface describes an alert.
+ *
+ * The interface is used by the {@link AlertService} to create alerts and work
+ * with the {@link AlertServiceSubscriber} which use/show the alerts.
+ */
 export interface Alert {
+  /**
+   * The message of the alert.
+   */
   message: string;
+
+  /**
+   * The size of the alert.
+   */
   size: string;
+
+  /**
+   * The color of the alert.
+   */
   color: string;
 }
