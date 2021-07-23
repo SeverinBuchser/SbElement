246c69ac4a66a1ffec067d73eec4e4042bedc03c
Severin Buchser <severin.buchser@gmx.ch>
2021-07-23 17:30:21 +0200

    Update documentation for column information interface

--- projects/sb-element/src/lib/models/table/column-information.interface.ts
+++ projects/sb-element/src/lib/models/table/column-information.interface.ts
@@ -1,8 +1,24 @@
 import { ColumnInformationOptionsInterface } from "./column-information-options.interface";
 
+/**
+ * Interface which must have all the fields for the column information. The
+ * interface is beeing used to make sure that all the fields are acessible and
+ * defined.
+ */
 export interface ColumnInformationInterface
   extends ColumnInformationOptionsInterface {
-  name: string;
-  color: string;
-  alignment: string;
+    /**
+     * The name of the column.
+     */
+    name: string;
+
+    /**
+     * The `color` of the column.
+     */
+    color: string;
+
+    /**
+     * The `alignment` of the column.
+     */
+    alignment: string;
 }
