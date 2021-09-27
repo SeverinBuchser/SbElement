0b8c4d0714299cf1603321177a88415ac94175ed
Severin Buchser <severin.buchser@gmx.ch>
2021-09-28 01:03:24 +0200

    Implement calendar month generation and update

--- projects/sb-element/src/lib/components/form/date-picker/date-picker-popper.component.ts
+++ projects/sb-element/src/lib/components/form/date-picker/date-picker-popper.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import * as fns from "date-fns";
 import { PopperDirective } from "../../popper/popper.directive";
+
 
 @Component({
   selector: 'sb-el-date-picker-popper',
@@ -7,15 +9,51 @@ import { PopperDirective } from "../../popper/popper.directive";
 })
 export class DatePickerPopperComponent extends PopperDirective {
 
+  public rootClass = 'sb-el-date-picker-popper';
+
   @Output()
   public select: EventEmitter<string> = new EventEmitter<string>();
-  public handleSelect(): void {this.select.emit("2021-12-09")}
+  public handleSelect(date: Date): void {
+    if (fns.isSameMonth(date, this.dateObject)) {
+      this.select.emit(fns.format(date, 'yyyy-MM-dd'));
+    }
+  }
+
+  private dateObject: Date = new Date();
+  public calendarMonth!: Array<Array<Date>>;
+
 
   @Input()
-  public date: string | undefined = '';
+  set date(dateString: string | undefined) {
+    if (dateString) {
+      this.dateObject = fns.parseISO(dateString);
+      this.updateCalendar();
+    }
+  };
+
+  get dateFormat(): string {return fns.format(this.dateObject, 'dd MMM. yyyy');}
 
   constructor() {
     super();
+    this.updateCalendar();
+  }
+
+  private updateCalendar(): void {
+    this.calendarMonth = new Array<Array<Date>>();
+    let calendarMonthStart = fns.addDays(fns.startOfWeek(fns.startOfMonth(
+      this.dateObject)), 1);
+
+    for (let week = 0 ; week < 6 ; week++) {
+      let calendarWeek = new Array<Date>();
+      for (let day = 0 ; day < 7 ; day++) {
+        calendarWeek.push(fns.addDays(calendarMonthStart, week * 7 + day));
+      }
+      this.calendarMonth.push(calendarWeek);
+    }
+  }
+
+  public format(date: Date): string {
+    return fns.format(date, 'dd');
   }
 
 }
