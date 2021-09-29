ada1eeb3af565120694830446cf675d872ab95d6
Severin Buchser <severin.buchser@gmx.ch>
2021-09-29 23:09:25 +0200

    Make color of icon in input text color and fix update of values

--- projects/sb-element/src/lib/components/form/date-picker/date-picker.component.ts
+++ projects/sb-element/src/lib/components/form/date-picker/date-picker.component.ts
@@ -5,7 +5,7 @@ import { DatePickerPopperComponent } from "./date-picker-popper.component";
 import { ControlValueAccessorSizeThemeColorInputDirective } from "../../base/control-value-accessor-style-input/control-value-accessor-size-theme-color-input.directive";
 import { ThemeService } from "../../../services/theme/theme.service";
 import { NG_VALUE_ACCESSOR } from "@angular/forms";
-import { isValid, parseISO } from "date-fns";
+import * as fns from "date-fns";
 
 @Component({
   selector: 'sb-el-date-picker',
@@ -20,26 +20,28 @@ export class DatePickerComponent extends ControlValueAccessorSizeThemeColorInput
 
   @ViewChild(PopoverTriggerClickDirective)
   private trigger!: PopoverTriggerClickDirective
-
   private popper?: ComponentRef<DatePickerPopperComponent>;
 
   constructor(
-    private poppverService: PopperService,
+    private popperService: PopperService,
     themeService: ThemeService
   ) {
     super(themeService);
   }
 
   open(): void {
-    this.popper = this.poppverService.pop(DatePickerPopperComponent, this.trigger);
+    this.popper = this.popperService.pop(DatePickerPopperComponent, this.trigger);
     this.popper.instance.select.subscribe((date: string) => this.handleSelect(date));
     this.popper.instance.date = this.value;
   }
 
   public handleSelect(date: string): void {
-    const parsedDate = parseISO(date);
-    const isValidDate = isValid(parsedDate);
-    if (isValidDate) this.writeValue(date);
+    const parsedDate = fns.parseISO(date);
+    const isValidDate = fns.isValid(parsedDate);
+    if (isValidDate) {
+      this.writeValueInnerChange(date);
+      this.updateValues();
+    }
   }
 
   protected updateValues(): void {
--- projects/sb-element/src/lib/components/form/input/input.component.ts
+++ projects/sb-element/src/lib/components/form/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Attribute, Component, Input, Optional } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ThemeService } from '../../../services/theme/theme.service';
 import { ControlValueAccessorSizeThemeColorInputDirective } from '../../base/control-value-accessor-style-input/control-value-accessor-size-theme-color-input.directive';
@@ -25,9 +25,6 @@ export class InputComponent extends ControlValueAccessorSizeThemeColorInputDirec
   @Input()
   public spellcheck: boolean = false;
 
-  public plain: boolean = false;
-  public pill: boolean = false;
-
   @Input()
   public prefixIcon: string = '';
   @Input()
@@ -35,13 +32,9 @@ export class InputComponent extends ControlValueAccessorSizeThemeColorInputDirec
 
 
   constructor(
-    @Optional() @Attribute('pill') pill: any,
-    @Optional() @Attribute('plain') plain: any,
     themeService: ThemeService
   ) {
     super(themeService);
-    if (pill === '') this.pill = true;
-    if (plain === '') this.plain = true;
   }
 
   public getInputClasses(): Array<string> {
@@ -54,14 +47,6 @@ export class InputComponent extends ControlValueAccessorSizeThemeColorInputDirec
     let classes = new Array<string>();
     classes.push(this.rootClass + '__placeholder')
     classes.push(this.value || this.focused ? 'is-top' : '')
-    return classes;
-  }
-
-
-  public getClasses(): Array<string> {
-    let classes = super.getClasses();
-    classes.push(this.pill ? 'is-pill' : '');
-    classes.push(this.plain ? 'is-plain': '');
     return classes;
   }
 
