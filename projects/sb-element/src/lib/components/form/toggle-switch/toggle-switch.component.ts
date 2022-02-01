fe374589cbffe24ae82cdb0b8d38bdc6244743d4
Severin Buchser <severin.buchser@gmx.ch>
2022-02-01 04:24:55 +0100

    Refactor toggle switch components style

--- projects/sb-element/src/lib/components/form/toggle-switch/toggle-switch.component.ts
+++ projects/sb-element/src/lib/components/form/toggle-switch/toggle-switch.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ControlValueAccessorSizeThemeColorInputDirective } from '../../../core/control-value-accessor-style-input/control-value-accessor-size-theme-color-input.directive';
 
 @Component({
   selector: 'sb-toggle-switch',
   templateUrl: './toggle-switch.component.html',
+  styleUrls: ['./toggle-switch.component.scss'],
+  encapsulation: ViewEncapsulation.None,
   providers: [{
     provide: NG_VALUE_ACCESSOR,
     useExisting: ToggleSwitchComponent,
