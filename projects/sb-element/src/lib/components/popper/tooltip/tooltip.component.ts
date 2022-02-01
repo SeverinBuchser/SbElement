11ec905635c29bbaa37fa1a384d58650a50f689e
Severin Buchser <severin.buchser@gmx.ch>
2022-02-01 03:51:27 +0100

    Refactor tooltip components style

--- projects/sb-element/src/lib/components/popper/tooltip/tooltip.component.ts
+++ projects/sb-element/src/lib/components/popper/tooltip/tooltip.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { PopperPosition } from "../popper/popper-position";
 import { ClassNameInputDirective } from '../../../core/';
 
 @Component({
   selector: 'sb-tooltip',
-  templateUrl: './tooltip.component.html'
+  templateUrl: './tooltip.component.html',
+  styleUrls: ['./tooltip.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TooltipComponent extends ClassNameInputDirective {
 
