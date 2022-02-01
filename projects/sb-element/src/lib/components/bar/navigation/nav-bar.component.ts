1cae280731962899e70d9b703e625644e358ab9c
Severin Buchser <severin.buchser@gmx.ch>
2022-02-01 04:07:18 +0100

    Refactor nav-bar components style

--- projects/sb-element/src/lib/components/bar/navigation/nav-bar.component.ts
+++ projects/sb-element/src/lib/components/bar/navigation/nav-bar.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { ClassNameInputDirective } from "../../../core";
 
 @Component({
   selector: 'sb-nav-bar',
-  templateUrl: './nav-bar.component.html'
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NavBarComponent extends ClassNameInputDirective {
   public rootClass: string = 'sb-nav-bar';
