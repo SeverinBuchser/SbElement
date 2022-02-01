0860f221ac19b7161081cb45ced1c7891c491868
Severin Buchser <severin.buchser@gmx.ch>
2022-02-01 04:40:43 +0100

    Refactor checkbox components style

--- projects/sb-element/src/lib/components/form/checkbox/checkbox.component.ts
+++ projects/sb-element/src/lib/components/form/checkbox/checkbox.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ControlValueAccessorSizeThemeColorInputDirective } from '../../../core/control-value-accessor-style-input/control-value-accessor-size-theme-color-input.directive';
 
 @Component({
   selector: 'sb-checkbox',
   templateUrl: './checkbox.component.html',
+  styleUrls: ['./checkbox.component.scss'],
+  encapsulation: ViewEncapsulation.None,
   providers: [{
     provide: NG_VALUE_ACCESSOR,
     useExisting: CheckboxComponent,
