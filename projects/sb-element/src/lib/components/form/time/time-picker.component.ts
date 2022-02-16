c0c5a630702dbe0c10d5ad2acef6514156ad7244
Severin Buchser <severin.buchser@gmx.ch>
2022-02-16 02:29:37 +0100

    Fix directory miss-happening and update time-picker

--- projects/sb-element/src/lib/components/form/time/time-picker.component.ts
+++ projects/sb-element/src/lib/components/form/time/time-picker.component.ts
@@ -1,16 +1,57 @@
-import { Component } from '@angular/core';
-import { ControlValueAccessorSizeThemeColorInputDirective } from "../../../core/control-value-accessor-style-input/control-value-accessor-size-theme-color-input.directive";
-import { NG_VALUE_ACCESSOR } from "@angular/forms";
+import { Component, Input } from '@angular/core';
+import { Color, mixinDisable, mixinFocus, Size } from "../../../core";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
+import { SbDoubleInput } from "../input";
+
+const SbTimePickerCore = mixinDisable(mixinFocus(class {}));
 
 @Component({
   selector: 'sb-input[type=time]',
   templateUrl: './time-picker.component.html',
+  inputs: [
+    'disabled'
+  ],
+  outputs: [
+    'focus',
+    'blur'
+  ],
   providers: [{
     provide: NG_VALUE_ACCESSOR,
     useExisting: TimePickerComponent,
     multi: true
   }]
 })
-export class TimePickerComponent extends ControlValueAccessorSizeThemeColorInputDirective<string> {
+export class TimePickerComponent extends SbTimePickerCore implements ControlValueAccessor {
 
+  @Input()
+  public color: string = Color.PRIMARY;
+
+  @Input()
+  public size: string = Size.DEFAULT;
+
+
+  private onChange: any = () => {};
+  private onTouch: any = () => {};
+
+  private innerValue: SbDoubleInput<number> = new SbDoubleInput();
+
+  set value(value: SbDoubleInput<number>) {
+    this.writeValue(value);
+    this.onChange(value);
+  }
+
+  get value(): SbDoubleInput<number> {
+    return this.innerValue;
+  }
+
+
+  public writeValue(value: SbDoubleInput<number>): void {
+    if (value !== this.innerValue && !this.disabled) {
+      this.innerValue = value;
+    }
+  }
+
+  public registerOnChange(fn: any): void { this.onChange = fn }
+  public registerOnTouched(fn: any): void { this.onTouch = fn }
+  public onBlur(): void { this.onTouch() }
 }
