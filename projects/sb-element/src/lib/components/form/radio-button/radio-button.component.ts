3da18414eafea7a8b8fdd45ab9e5f3ee281eee29
Severin Buchser <severin.buchser@gmx.ch>
2022-02-18 15:08:08 +0100

    Update radio button

--- projects/sb-element/src/lib/components/form/radio-button/radio-button.component.ts
+++ projects/sb-element/src/lib/components/form/radio-button/radio-button.component.ts
@@ -1,28 +1,88 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
-import { ControlValueAccessorThemeColorInputDirective } from '../../../core';
+import { Component, ElementRef, Input, ViewEncapsulation } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Color, mixinClassName, mixinColor, mixinDisable, mixinFocus, mixinTheme, ThemeService } from '../../../core';
+
+const SbRadioButtonCore = mixinDisable(
+  mixinFocus(
+    mixinColor(
+      mixinTheme(
+        mixinClassName(
+          class {
+            constructor(
+              public _elementRef: ElementRef,
+              public _themeService: ThemeService) {}
+          }, 'sb-radio'
+        )
+      ), Color.PRIMARY
+    )
+  )
+);
 
 @Component({
   selector: 'sb-radio-button',
   templateUrl: './radio-button.component.html',
   styleUrls: ['./radio-button.component.scss'],
   encapsulation: ViewEncapsulation.None,
+  host: {
+    '[class.disabled]': 'disabeld'
+  },
+  inputs: [
+    'size',
+    'color',
+    'disabled'
+  ],
+  outputs: [
+    'focus',
+    'blur'
+  ],
   providers: [{
     provide: NG_VALUE_ACCESSOR,
     useExisting: RadioButtonComponent,
     multi: true
   }]
 })
-export class RadioButtonComponent extends ControlValueAccessorThemeColorInputDirective<string> {
-
-  public rootClass = 'sb-radio';
-
-  private static id: number = 0;
+export class RadioButtonComponent extends SbRadioButtonCore implements ControlValueAccessor {
 
   @Input()
   public name: string = '';
 
   @Input()
   public label: string = '';
+
+  private onChange: any = () => {};
+  private onTouch: any = () => {};
+
+  private innerValue: string | undefined = undefined;
+
+  set value(value: string) {
+    if (value == '' || value !== this.innerValue && !this.disabled) {
+      this.innerValue = value;
+      this.onChange(value);
+    }
+  }
+
+  get value(): string {
+    if (this.innerValue) {
+      return this.innerValue;
+    } else return '';
+  }
+
+  constructor(
+    elementRef: ElementRef,
+    themeService: ThemeService
+  ) {
+    super(elementRef, themeService);
+  }
+
+
+  public writeValue(value: string): void {
+    if (value == '' || value !== this.innerValue && !this.disabled) {
+      this.innerValue = value;
+    }
+  }
+
+  public registerOnChange(fn: any): void { this.onChange = fn }
+  public registerOnTouched(fn: any): void { this.onTouch = fn }
+  public onBlur(): void { this.onTouch() }
 
 }
