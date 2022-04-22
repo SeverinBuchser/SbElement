f89855f78206b20d28119b1882df07714b2d4e18
Severin Buchser <severin.buchser@gmx.ch>
2022-04-22 14:39:11 +0200

    Update expansion card and fix example encapsulation

--- example-app/src/app/example/calendar/calendar-example/calendar-example.component.ts
+++ example-app/src/app/example/calendar/calendar-example/calendar-example.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-calendar-example',
   templateUrl: './calendar-example.component.html',
-  styleUrls: ['./calendar-example.component.scss']
+  styleUrls: ['./calendar-example.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CalendarExampleComponent implements OnInit {
 
--- example-app/src/app/example/indicator/indicator-example/indicator-example.component.ts
+++ example-app/src/app/example/indicator/indicator-example/indicator-example.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-indicator-example',
   templateUrl: './indicator-example.component.html',
-  styleUrls: ['./indicator-example.component.scss']
+  styleUrls: ['./indicator-example.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class IndicatorExampleComponent implements OnInit {
 
